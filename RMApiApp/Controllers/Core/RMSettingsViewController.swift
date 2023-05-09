fd0636cb0887f7aac8261d69a74a14f7e9d9228c
Namghjk <namsgu123@gmail.com>
2023-05-09 14:19:24 +0700

    add Prompt RatingApp & AppIcon, Launch Image

--- RMApiApp/Controllers/Core/RMSettingsViewController.swift
+++ RMApiApp/Controllers/Core/RMSettingsViewController.swift
@@ -5,14 +5,15 @@
 //  Created by Nam Pham on 15/03/2023.
 //
 
+import StoreKit
 import SafariServices
 import SwiftUI
 import UIKit
 
 /// Controller to show Setting
- class RMSettingsViewController: UIViewController {
+class RMSettingsViewController: UIViewController {
     
-     private var settingsSwiftUIController: UIHostingController<RMSettingsView>? = nil
+    private var settingsSwiftUIController: UIHostingController<RMSettingsView>? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,8 +30,8 @@ import UIKit
                         return RMSettingsCellViewVM(type: $0) { [weak self] option in
                             self?.handleTap(option: option)
                         }
-        })
-    )))
+                    })
+                )))
         addChild(settingsSwiftUIController)
         settingsSwiftUIController.didMove(toParent: self)
         
@@ -47,17 +48,25 @@ import UIKit
         
         self.settingsSwiftUIController = settingsSwiftUIController
     }
-     private func handleTap(option:RMSettingsOption) {
-         guard Thread.current.isMainThread else {
-             return
-         }
-         
-         if let url = option.targetURL {
-             //open website
-             let vc = SFSafariViewController(url: url)
-             present(vc, animated: true)
-         }else if option == .rateApp {
-             //Show rating prompt
-         }
-     }
+    private func handleTap(option:RMSettingsOption) {
+        guard Thread.current.isMainThread else {
+            return
+        }
+        
+        if let url = option.targetURL {
+            //open website
+            let vc = SFSafariViewController(url: url)
+            present(vc, animated: true)
+        }else if option == .rateApp {
+            //Show rating prompt
+            
+            
+            if let windowScene = view.window?.windowScene{
+                SKStoreReviewController.requestReview(in: windowScene)
+            }
+            
+            
+        }
+        
+    }
 }
